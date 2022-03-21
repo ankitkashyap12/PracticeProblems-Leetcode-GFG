78a3f1340c5e07d20372ca077f20e8961ec82ed3
Ankit Kashyap <ankitkashyap367@gmail.com>
2022-03-21 16:17:57 +0530

    added cooments for understanding

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,3 +1,12 @@
+/* Kadane's Algo
+
+The idea is simple: Using a variable (currSum) track the sum of the elements. If at any instance, this sum is negative, make it 0. This is because, for the next value, there are only 2 options: Either the next value will be positive or negative.
+
+If the next value is positive: Then, the running sum (-ve) + next value (+ve) will offcourse be greater than current sum, as +ve is always greater than -ve.
+If the next value is negative: Then, the running sum (-ve) + next value (also -ve) will become higher value in negative, which will be smaller.
+Hence, if by adding an element the total sum becomes negative then, that element cannot be considered in maximum subarray and we should start a new subarray.
+    */
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum=0;
