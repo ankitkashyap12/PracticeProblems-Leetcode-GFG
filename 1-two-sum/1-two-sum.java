f6dcdb39b29be897ccb71f5ab4dac741f639de0a
Ankit Kashyap <ankitkashyap367@gmail.com>
2022-03-21 16:33:31 +0530

    Time: 34 ms (43.09%), Space: 42.2 MB (86.08%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] indices=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int diff = target-nums[i];
+            indices[0]=i;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]==diff){
+                    indices[1]=j;
+                    return indices;
+                }
+                
+            }
+        }
+        return indices;
+}
+}
