e40fe7141b6710337eb3ffde8ab1b87057c3b446
Ankit Kashyap <ankitkashyap367@gmail.com>
2026-03-04 01:17:47 +0530

    Time: 3 ms (61.59%), Space: 43.6 MB (99.88%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right= s.length()-1;
+        s=s.toLowerCase();
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;  
+            }
+            char currLeft=Character.toLowerCase(s.charAt(left));
+            char currRight = Character.toLowerCase(s.charAt(right));
+            if(currLeft!=currRight)
+                return false;
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+}
